93dc0af20965e0d4beb39bc1b1e01c4912525367
Stefan Thomas <justmoon@members.fsf.org>
2011-05-07 02:22:46 +0100

    Fixed/cleaned test/util.js

--- test/util.js
+++ test/util.js
@@ -1,44 +1,29 @@
-var path = require('path');
-require.paths.unshift(path.join(__dirname, '..'));
-
 var vows = require('vows'),
     assert = require('assert');
 
-var Util = require('lib/util');
+var Util = require('../lib/util');
 
-var suite = vows.describe('util');
+vows.describe('Bitcoin Utils').addBatch({
+	'A Bitcoin address': {
+		topic: "12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX",
+		'is decoded correctly': function (topic) {
+			var addrHash = Util.addressToPubKeyHash(topic);
 
-suite.addBatch({
-	'Bitcoin addresses': {
-		'decode a bitcoin address': {
-			topic: "12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX",
-
-			'and check the result': function (topic) {
-				var addrHash = Util.addressToPubKeyHash(topic);
-				assert.equal(Util.pubKeyHashToAddress(addrHash), topic);
-			}
+			var expected = new Buffer('119b098e2e980a229e139a9ed01a469e518e6f26', 'hex');
+			assert.equal(expected.compare(addrHash), 0);
 		},
-		'encode a bitcoin address': {
-			topic: new Buffer("119b098e2e980a229e139a9ed01a469e518e6f26", 'hex'),
-
-			'and check the result': function (topic) {
-				var addrHash = Util.pubKeyHashToAddress(topic);
-				assert.equal(Util.addressToPubKeyHash(addrHash).compare(topic), 0);
-			}
+		'is re-encoded correctly': function (topic) {
+			var addrHash = Util.addressToPubKeyHash(topic);
+			assert.equal(Util.pubKeyHashToAddress(addrHash), topic);
 		}
 	},
 
-	'difficulty': {
-		'calculate target from bits': {
-			topic: 0x1b0404cb,
-
-			'and check the result': function (topic) {
-				var target = Util.decodeDiffBits(topic);
-				assert.equal(target.toHex(),
-							"00000000000404cb000000000000000000000000000000000000000000000000");
-			}
+	'Difficulty bits': {
+		topic: 0x1b0404cb,
+		'can be converted to a target': function (topic) {
+			var target = Util.decodeDiffBits(topic);
+			assert.equal(target.toHex(),
+						 "00000000000404cb000000000000000000000000000000000000000000000000");
 		}
 	}
-});
-
-suite.run();
+}).export(module);
