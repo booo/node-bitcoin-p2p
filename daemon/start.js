e08533405a8d2486ff42b2cb261e63e313e527ac
Stefan Thomas <justmoon@members.fsf.org>
2011-07-02 17:47:16 +0100

    More daemon infrastructure. (Config file, welcome message, etc.)

--- daemon/start.js
+++ daemon/start.js
@@ -1,12 +1,37 @@
+#!/usr/bin/env node
+
+var sys = require('sys');
+var path = require('path');
+
 // Load node-bitcoin-p2p
 var Bitcoin = require('../lib/bitcoin');
 var logger = require('../lib/logger');
 
+// Print welcome message
+require("./welcome");
+
 // Load user-defined settings
 try {
+  logger.info('Loading configuration');
   var cfg = require('./settings');
 } catch (e) {
-  
+  if (e.message == "Cannot find module './settings'") {
+    logger.error('Configuration file not found!');
+    sys.puts(
+      "\n" +
+      "  We were looking under:\n" +
+      "  " + path.resolve(__dirname, './settings.js') + "\n" +
+      "\n" +
+      "  If you just installed node-bitcoin-p2p, this is normal.\n" +
+      "  You'll find an example config file here:\n" +
+      "  " + path.resolve(__dirname, './settings.example.js') + "\n" +
+      "\n" +
+      "  Simply edit it and copy it over!\n");
+    logger.info('Exiting...');
+    process.exit(1);
+  } else {
+    throw e;
+  }
 }
 
 // Start node
---
+++ daemon/welcome.js
@@ -0,0 +1,18 @@
+var sys = require('sys');
+var Bitcoin = require('../lib/bitcoin');
+
+var version = ""+Bitcoin.version;
+while (version.length < 11) {
+  version += " ";
+}
+
+sys.puts(
+  "\n" +
+  "BitcoinJS Daemon v"+version+"                           -+syys/.    \n" +
+  "(powered by node-bitcoin-p2p)                          +ddo++hdh-   \n" +
+  "                                                     `:ydd+//odhs/:`\n" +
+  "Copyright (c) 2011 BitcoinJS Project                 +--+so/+s+/.`y:\n" +
+  "http://bitcoinjs.org/                                `/+++++++++++: \n" +
+  "\n" +
+  "This program is free software; you can redistribute it and/or modify\n" +
+  "it under terms of the MIT license.\n");
