9e8a73f792d80a55bc1988738f644651bb08cd47
Stefan Thomas <justmoon@members.fsf.org>
2011-07-06 02:23:01 +0100

    Allow custom path to config file via parameter.

--- daemon/start.js
+++ daemon/start.js
@@ -2,33 +2,40 @@
 
 var sys = require('sys');
 var path = require('path');
+var opts = require('opts');
 
 // Load node-bitcoin-p2p
 var Bitcoin = require('../lib/bitcoin');
 var logger = require('../lib/logger');
 
+// Command-line arguments parsing
+var options = [{
+  short: 'c',
+  long: 'config',
+  description: 'Configuration file',
+  value: true
+}];
+opts.parse(options, true);
+
 // Print welcome message
 require("./welcome");
 
 // Load user-defined settings
+logger.info('Loading configuration');
 try {
-  logger.info('Loading configuration');
-  var cfg = require('./settings');
+  var configPath = opts.get('config') || './settings';
+  var cfg = require(configPath);
 } catch (e) {
-  if (e.message == "Cannot find module './settings'") {
-    logger.error('Configuration file not found!');
+  if (/^Cannot find module /.test(e.message)) {
+    logger.warn('No configuration file found!');
     sys.puts(
       "\n" +
-      "  We were looking under:\n" +
-      "  " + path.resolve(__dirname, './settings.js') + "\n" +
+      "BitcoinJS was unable to locate your config file under:\n" +
+      "" + path.resolve(__dirname, configPath) + "\n" +
       "\n" +
-      "  If you just installed node-bitcoin-p2p, this is normal.\n" +
-      "  You'll find an example config file here:\n" +
-      "  " + path.resolve(__dirname, './settings.example.js') + "\n" +
-      "\n" +
-      "  Simply edit it and copy it over!\n");
-    logger.info('Exiting...');
-    process.exit(1);
+      "If you just installed node-bitcoin-p2p, this is normal.\n" +
+      "You'll find an example config file here:\n" +
+      "" + path.resolve(__dirname, './settings.example.js') + "\n");
   } else {
     throw e;
   }
