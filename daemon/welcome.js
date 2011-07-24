eb5c8318044f55fe384677231a46bac9ebd6d7b9
Stefan Thomas <justmoon@members.fsf.org>
2011-07-25 00:01:09 +0100

    Now with even cuter ASCII art!!!!11

--- daemon/welcome.js
+++ daemon/welcome.js
@@ -1,18 +1,29 @@
 var sys = require('sys');
 var Bitcoin = require('../lib/bitcoin');
 
-var version = ""+Bitcoin.version;
-while (version.length < 11) {
-  version += " ";
-}
+var asciiArt = [
+  "  ,d8b."
+, " ,8BTC8-."
+, "( 'Y8Y'.:)"
+, " `-`-`--'"];
 
-sys.puts(
+var message =
+  "BitcoinJS Daemon v"+Bitcoin.version+" (node-bitcoin-p2p)\n" +
   "\n" +
-  "BitcoinJS Daemon v"+version+"                           -+syys/.    \n" +
-  "(powered by node-bitcoin-p2p)                          +ddo++hdh-   \n" +
-  "                                                     `:ydd+//odhs/:`\n" +
-  "Copyright (c) 2011 BitcoinJS Project                 +--+so/+s+/.`y:\n" +
-  "http://bitcoinjs.org/                                `/+++++++++++: \n" +
+  "Copyright (c) 2011 BitcoinJS Project\n" +
+  "http://bitcoinjs.org/\n" +
   "\n" +
   "This program is free software; you can redistribute it and/or modify\n" +
-  "it under the terms of the MIT license.\n");
+  "it under the terms of the MIT license.\n";
+
+var pos = 12;
+message = message.split('\n').map(function (line, i) {
+  if (i >= 0 && i <= 3) {
+    var ascii = " "+asciiArt[i];
+    while (ascii.length < pos) ascii += " ";
+    line = ascii + line;
+  }
+  return line;
+}).join('\n');
+
+sys.puts("\n"+message);
