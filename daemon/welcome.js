7fbc610b8c0a70b0153c2e9eaa70526be4d6d97d
Stefan Thomas <justmoon@members.fsf.org>
2011-09-07 06:48:07 +0100

    Updated launch message. Removed ASCII art.

--- daemon/welcome.js
+++ daemon/welcome.js
@@ -1,29 +1,14 @@
 var sys = require('sys');
 var Bitcoin = require('../lib/bitcoin');
+require('colors');
 
-var asciiArt = [
-  "  ,d8b."
-, " ,8BTC8-."
-, "( 'Y8Y'.:)"
-, " `-`-`--'"];
+var asciiArt = [" (" + "(".grey + "(B)".yellow + ")".grey + ") "];
 
 var message =
-  "BitcoinJS Daemon v"+Bitcoin.version+" (node-bitcoin-p2p)\n" +
+  asciiArt+"BitcoinJS Daemon v"+Bitcoin.version+" (node-bitcoin-p2p)\n" +
+  "         Copyright (c) 2011 BitcoinJS Project\n" +
   "\n" +
-  "Copyright (c) 2011 BitcoinJS Project\n" +
-  "http://bitcoinjs.org/\n" +
-  "\n" +
-  "This program is free software; you can redistribute it and/or modify\n" +
-  "it under the terms of the MIT license.\n";
-
-var pos = 12;
-message = message.split('\n').map(function (line, i) {
-  if (i >= 0 && i <= 3) {
-    var ascii = " "+asciiArt[i];
-    while (ascii.length < pos) ascii += " ";
-    line = ascii + line;
-  }
-  return line;
-}).join('\n');
+  " LICENSE This program is free software; you can redistribute it and/or modify\n" +
+  "         it under the terms of the MIT license.\n";
 
 sys.puts("\n"+message);
