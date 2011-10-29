951607f15bd86e1706276028e37438ae26f3b59b
Stefan Thomas <justmoon@members.fsf.org>
2011-10-29 12:03:04 +0200

    Refactored native code. Added async BitcoinKey.verifySignature().

--- native.cc
+++ native.cc
@@ -21,132 +21,466 @@ using namespace std;
 using namespace v8;
 using namespace node;
 
-
+#define REQ_FUN_ARG(I, VAR)                                                       \
+  if (args.Length() <= (I) || !args[I]->IsFunction())                             \
+    return ThrowException(Exception::TypeError(                                   \
+                            String::New("Argument " #I " must be a function")));  \
+  Local<Function> VAR = Local<Function>::Cast(args[I]);
 
 static Handle<Value> VException(const char *msg) {
     HandleScope scope;
     return ThrowException(Exception::Error(String::New(msg)));
 }
 
-
-static Handle<Value>
-new_keypair (const Arguments& args)
+class BitcoinKey : ObjectWrap
 {
-  HandleScope scope;
-  EC_KEY* pkey;
+private:
+
+  const char *lastError;
+  EC_KEY *ec;
+
+  bool hasPrivate;
+  bool hasPublic;
+
+  static BitcoinKey *Generate()
+  {
+    BitcoinKey *key = new BitcoinKey();
+    if (key->lastError) {
+      return key;
+    }
+
+    if (!EC_KEY_generate_key(key->ec)) {
+      key->lastError = "Error from EC_KEY_generate_key";
+      return key;
+    }
+
+    key->hasPublic = true;
+    key->hasPrivate = true;
+
+    return key;
+  }
+
+  struct verify_sig_baton_t {
+    // Parameters
+    BitcoinKey *key;
+    const unsigned char *digest;
+    int digest_len;
+    const unsigned char *sig;
+    int sig_len;
+
+    // Result
+    // -1 = error, 0 = bad sig, 1 = good
+    int result;
+    Persistent<Function> cb;
+  };
+
+  int VerifySignature(const unsigned char *digest, int digest_len,
+                      const unsigned char *sig, int sig_len)
+  {
+    return ECDSA_verify(0, digest, digest_len, sig, sig_len, ec);
+  }
+
+  static int EIO_VerifySignature(eio_req *req)
+  {
+    verify_sig_baton_t *b = static_cast<verify_sig_baton_t *>(req->data);
+
+    b->result = b->key->VerifySignature(b->digest, b->digest_len,
+                                        b->sig, b->sig_len);
+
+    return 0;
+  }
+
+public:
+
+  static Persistent<FunctionTemplate> s_ct;
+  static void Init(Handle<Object> target)
+  {
+    HandleScope scope;
+    Local<FunctionTemplate> t = FunctionTemplate::New(New);
+
+    s_ct = Persistent<FunctionTemplate>::New(t);
+    s_ct->InstanceTemplate()->SetInternalFieldCount(1);
+    s_ct->SetClassName(String::NewSymbol("BitcoinKey"));
+
+    // Accessors
+    s_ct->InstanceTemplate()->SetAccessor(String::New("private"),
+                                          GetPrivate, SetPrivate);
+    s_ct->InstanceTemplate()->SetAccessor(String::New("public"),
+                                          GetPublic, SetPublic);
+
+    // Methods
+    NODE_SET_PROTOTYPE_METHOD(s_ct, "verifySignature", VerifySignature);
+    NODE_SET_PROTOTYPE_METHOD(s_ct, "verifySignatureSync", VerifySignatureSync);
+    NODE_SET_PROTOTYPE_METHOD(s_ct, "toDER", ToDER);
+
+    // Static methods
+    NODE_SET_METHOD(s_ct->GetFunction(), "generateSync", GenerateSync);
+    NODE_SET_METHOD(s_ct->GetFunction(), "fromDER", FromDER);
+
+    target->Set(String::NewSymbol("BitcoinKey"),
+                s_ct->GetFunction());
+  }
+
+  BitcoinKey() :
+    lastError(NULL)
+  {
+    ec = EC_KEY_new_by_curve_name(NID_secp256k1);
+    if (ec == NULL) {
+      lastError = "Error from EC_KEY_new_by_curve_name";
+    }
+  }
+
+  ~BitcoinKey()
+  {
+    EC_KEY_free(ec);
+  }
+
+  static Handle<Value>
+  New(const Arguments& args)
+  {
+    HandleScope scope;
+
+    // If an "External" is passed in as the argument, wrap it, otherwise
+    // instantiate a new C++ object. Credit goes to Christian Plesner Hansen
+    // for this method.
+    if (args[0]->IsExternal()) {
+      Handle<External> external = Handle<External>::Cast(args[0]);
+      args.This()->SetInternalField(0, external);
+    } else {
+      BitcoinKey* key = new BitcoinKey();
+      if (key->lastError != NULL) {
+        return VException(key->lastError);
+      }
+
+      key->Wrap(args.This());
+    }
+    return args.This();
+  }
+
+  static Handle<Value>
+  GenerateSync(const Arguments& args)
+  {
+    HandleScope scope;
+
+    BitcoinKey* key = Generate();
+
+    if (key->lastError != NULL) {
+      return VException(key->lastError);
+    }
+
+    Handle<Function> cons = s_ct->GetFunction();
+    Handle<Value> external = External::New(key);
+    Handle<Value> result = cons->NewInstance(1, &external);
+
+    return scope.Close(result);
+  }
+
+  static Handle<Value>
+  GetPrivate(Local<String> property, const AccessorInfo& info)
+  {
+    HandleScope scope;
+    BitcoinKey* key = node::ObjectWrap::Unwrap<BitcoinKey>(info.Holder());
+
+    if (!key->hasPrivate) {
+      return scope.Close(Null());
+    }
+
+    const BIGNUM *bn = EC_KEY_get0_private_key(key->ec);
+    int priv_size = BN_num_bytes(bn);
+
+    if (bn == NULL) {
+      // TODO: ERROR: "Error from EC_KEY_get0_private_key(pkey)"
+      return scope.Close(Null());
+    }
+
+    if (priv_size > 32) {
+      // TODO: ERROR: "Secret too large (Incorrect curve parameters?)"
+      return scope.Close(Null());
+    }
+
+    unsigned char *priv = (unsigned char *)malloc(32);
+
+    int n = BN_bn2bin(bn, &priv[32 - priv_size]);
+
+    if (n != priv_size) {
+      // TODO: ERROR: "Error from BN_bn2bin(bn, &priv[32 - priv_size])"
+      return scope.Close(Null());
+    }
+
+    Buffer *priv_buf = Buffer::New(32);
+    memcpy(Buffer::Data(priv_buf), priv, 32);
+
+    free(priv);
+
+    return scope.Close(priv_buf->handle_);
+  }
+
+  static void
+  SetPrivate(Local<String> property, Local<Value> value, const AccessorInfo& info)
+  {
+    BitcoinKey* key = node::ObjectWrap::Unwrap<BitcoinKey>(info.Holder());
+    Handle<Object> buffer = value->ToObject();
+    const unsigned char *data = (const unsigned char*) Buffer::Data(buffer);
+
+    if (!d2i_ECPrivateKey(&(key->ec), &data, Buffer::Length(buffer))) {
+      // TODO: Error
+      return;
+    }
+
+    key->hasPrivate = true;
+  }
+
+  static Handle<Value>
+  GetPublic(Local<String> property, const AccessorInfo& info)
+  {
+    HandleScope scope;
+    BitcoinKey* key = node::ObjectWrap::Unwrap<BitcoinKey>(info.Holder());
+
+    if (!key->hasPublic) {
+      return scope.Close(Null());
+    }
+
+    // Export public
+    unsigned int pub_size = i2o_ECPublicKey(key->ec, NULL);
+    if (!pub_size) {
+      // TODO: ERROR: "Error from i2o_ECPublicKey(key->ec, NULL)"
+      return scope.Close(Null());
+    }
+    unsigned char *pub_begin, *pub_end;
+    pub_begin = pub_end = (unsigned char *)malloc(pub_size);
+
+    if (i2o_ECPublicKey(key->ec, &pub_end) != pub_size) {
+      // TODO: ERROR: "Error from i2o_ECPublicKey(key->ec, &pub)"
+      return scope.Close(Null());
+    }
+    Buffer *pub_buf = Buffer::New(pub_size);
+    memcpy(Buffer::Data(pub_buf), pub_begin, pub_size);
+
+    free(pub_begin);
+
+    return scope.Close(pub_buf->handle_);
+  }
+
+  static void
+  SetPublic(Local<String> property, Local<Value> value, const AccessorInfo& info)
+  {
+    BitcoinKey* key = node::ObjectWrap::Unwrap<BitcoinKey>(info.Holder());
+    Handle<Object> buffer = value->ToObject();
+    const unsigned char *data = (const unsigned char*) Buffer::Data(buffer);
+
+    if (!o2i_ECPublicKey(&(key->ec), &data, Buffer::Length(buffer))) {
+      // TODO: Error
+      return;
+    }
+
+    key->hasPublic = true;
+  }
+
+  static Handle<Value>
+  ToDER(const Arguments& args)
+  {
+    HandleScope scope;
+    BitcoinKey* key = node::ObjectWrap::Unwrap<BitcoinKey>(args.This());
+
+    if (!key->hasPrivate || !key->hasPublic) {
+      return scope.Close(Null());
+    }
+
+    // Export DER
+    unsigned int der_size = i2d_ECPrivateKey(key->ec, NULL);
+    if (!der_size) {
+      // TODO: ERROR: "Error from i2d_ECPrivateKey(key->ec, NULL)"
+      return scope.Close(Null());
+    }
+    unsigned char *der_begin, *der_end;
+    der_begin = der_end = (unsigned char *)malloc(der_size);
+
+    if (i2d_ECPrivateKey(key->ec, &der_end) != der_size) {
+      // TODO: ERROR: "Error from i2d_ECPrivateKey(key->ec, &der_end)"
+      return scope.Close(Null());
+    }
+    Buffer *der_buf = Buffer::New(der_size);
+    memcpy(Buffer::Data(der_buf), der_begin, der_size);
+
+    free(der_begin);
+
+    return scope.Close(der_buf->handle_);
+  }
+
+  static Handle<Value>
+  FromDER(const Arguments& args)
+  {
+    HandleScope scope;
+
+    if (args.Length() != 1) {
+      return VException("One argument expected: der");
+    }
+    if (!Buffer::HasInstance(args[0])) {
+      return VException("Argument 'der' must be of type Buffer");
+    }
+
+    BitcoinKey* key = new BitcoinKey();
+    if (key->lastError != NULL) {
+      return VException(key->lastError);
+    }
+
+    Handle<Object> der_buf = args[0]->ToObject();
+    const unsigned char *data = (const unsigned char*) Buffer::Data(der_buf);
+
+    if (!d2i_ECPrivateKey(&(key->ec), &data, Buffer::Length(der_buf))) {
+      return VException("Error from d2i_ECPrivateKey(&key, &data, len)");
+    }
+    
+    key->hasPrivate = true;
+    key->hasPublic = true;
+
+    Handle<Function> cons = s_ct->GetFunction();
+    Handle<Value> external = External::New(key);
+    Handle<Value> result = cons->NewInstance(1, &external);
+
+    return scope.Close(result);
+  }
+
+  static Handle<Value>
+  VerifySignature(const Arguments& args)
+  {
+    HandleScope scope;
+    BitcoinKey* key = node::ObjectWrap::Unwrap<BitcoinKey>(args.This());
   
-  // Generate
-  pkey = EC_KEY_new_by_curve_name(NID_secp256k1);
-  if (pkey == NULL) {
-    return VException("Error from EC_KEY_new_by_curve_name");
+    if (args.Length() != 3) {
+      return VException("Three arguments expected: hash, sig, callback");
+    }
+    if (!Buffer::HasInstance(args[0])) {
+      return VException("Argument 'hash' must be of type Buffer");
+    }
+    if (!Buffer::HasInstance(args[1])) {
+      return VException("Argument 'sig' must be of type Buffer");
+    }
+    REQ_FUN_ARG(2, cb);
+    if (!key->hasPublic) {
+      return VException("BitcoinKey does not have a public key set");
+    }
+
+    Handle<Object> hash_buf = args[0]->ToObject();
+    Handle<Object> sig_buf = args[1]->ToObject();
+
+    const unsigned char *hash_data = (unsigned char *) Buffer::Data(hash_buf);
+    const unsigned char *sig_data = (unsigned char *) Buffer::Data(sig_buf);
+
+    unsigned int hash_len = Buffer::Length(hash_buf);
+    unsigned int sig_len = Buffer::Length(sig_buf);
+
+    if (hash_len != 32) {
+      return VException("Argument 'hash' must be Buffer of length 32 bytes");
+    }
+
+    verify_sig_baton_t *baton = new verify_sig_baton_t();
+    baton->key = key;
+    baton->digest = hash_data;
+    baton->digest_len = hash_len;
+    baton->sig = sig_data;
+    baton->sig_len = sig_len;
+    baton->result = -1;
+    baton->cb = Persistent<Function>::New(cb);
+
+    key->Ref();
+
+    eio_custom(EIO_VerifySignature, EIO_PRI_DEFAULT, VerifySignatureCallback, baton);
+    ev_ref(EV_DEFAULT_UC);
+
+    return Undefined();
   }
-  if (!EC_KEY_generate_key(pkey)) {
-    return VException("Error from EC_KEY_generate_key");
+
+  static int
+  VerifySignatureCallback(eio_req *req)
+  {
+    HandleScope scope;
+    verify_sig_baton_t *baton = static_cast<verify_sig_baton_t *>(req->data);
+    ev_unref(EV_DEFAULT_UC);
+    baton->key->Unref();
+
+    Local<Value> argv[2];
+
+    argv[0] = Local<Value>::New(Null());
+    argv[1] = Local<Value>::New(Null());
+    if (baton->result == -1) {
+      argv[0] = Exception::TypeError(String::New("Error during ECDSA_verify"));
+    } else if (baton->result == 0) {
+      // Signature invalid
+      argv[1] = Local<Value>::New(Boolean::New(false));
+    } else if (baton->result == 1) {
+      // Signature valid
+      argv[1] = Local<Value>::New(Boolean::New(true));
+    } else {
+      argv[0] = Exception::TypeError(
+        String::New("ECDSA_verify gave undefined return value"));
+    }
+
+    TryCatch try_catch;
+
+    baton->cb->Call(Context::GetCurrent()->Global(), 2, argv);
+
+    if (try_catch.HasCaught()) {
+      FatalException(try_catch);
+    }
+
+    baton->cb.Dispose();
+
+    delete baton;
+    return 0;
   }
+
+  static Handle<Value>
+  VerifySignatureSync(const Arguments& args)
+  {
+    HandleScope scope;
+    BitcoinKey* key = node::ObjectWrap::Unwrap<BitcoinKey>(args.This());
   
-  // Export private
-  unsigned int priv_size = i2d_ECPrivateKey(pkey, NULL);
-  if (!priv_size) {
-    return VException("Error from i2d_ECPrivateKey(pkey, NULL)");
-  }
-  unsigned char *priv = (unsigned char *)malloc(priv_size);
-  if (i2d_ECPrivateKey(pkey, &priv) != priv_size) {
-   return VException("Error from i2d_ECPrivateKey(pkey, &priv)");
-  }
-  
-  // Export public
-  unsigned int pub_size = i2o_ECPublicKey(pkey, NULL);
-  if (!pub_size) {
-    return VException("Error from i2o_ECPublicKey(pkey, NULL)");
-  }
-  unsigned char *pub = (unsigned char *)malloc(pub_size);
-  if (i2o_ECPublicKey(pkey, &pub) != pub_size) {
-    return VException("Error from i2o_ECPublicKey(pkey, &pub)");
-  }
-  
-  // Return [priv_buf, pub_buf]
-  
-  Local<Array> a = Array::New(2);
-  
-  Buffer *priv_buf = Buffer::New(priv_size);
-  memcpy(Buffer::Data(priv_buf), priv, priv_size);
-  a->Set(Integer::New(0), priv_buf->handle_);
-  
-  Buffer *pub_buf = Buffer::New(pub_size);
-  memcpy(Buffer::Data(pub_buf), pub, pub_size);
-  a->Set(Integer::New(1), pub_buf->handle_);
-  
-  EC_KEY_free(pkey);
-  free(priv);
-  free(pub);
+    if (args.Length() != 2) {
+      return VException("Two arguments expected: hash, sig");
+    }
+    if (!Buffer::HasInstance(args[0])) {
+      return VException("Argument 'hash' must be of type Buffer");
+    }
+    if (!Buffer::HasInstance(args[1])) {
+      return VException("Argument 'sig' must be of type Buffer");
+    }
+    if (!key->hasPublic) {
+      return VException("BitcoinKey does not have a public key set");
+    }
 
-  return scope.Close(a);
-}
+    Handle<Object> hash_buf = args[0]->ToObject();
+    Handle<Object> sig_buf = args[1]->ToObject();
 
-static Handle<Value>
-verify_sig (const Arguments& args)
-{
-  HandleScope scope;
-  EC_KEY* pkey;
-  
-  if (args.Length() != 3) {
-    return VException("Three arguments expected: sig, pubkey, hash");
+    const unsigned char *hash_data = (unsigned char *) Buffer::Data(hash_buf);
+    const unsigned char *sig_data = (unsigned char *) Buffer::Data(sig_buf);
+
+    unsigned int hash_len = Buffer::Length(hash_buf);
+    unsigned int sig_len = Buffer::Length(sig_buf);
+
+    if (hash_len != 32) {
+      return VException("Argument 'hash' must be Buffer of length 32 bytes");
+    }
+
+    // Verify signature
+    int result = key->VerifySignature(hash_data, hash_len, sig_data, sig_len);
+
+    if (result == -1) {
+      return VException("Error during ECDSA_verify");
+    } else if (result == 0) {
+      // Signature invalid
+      return scope.Close(Boolean::New(false));
+    } else if (result == 1) {
+      // Signature valid
+      return scope.Close(Boolean::New(true));
+    } else {
+      return VException("ECDSA_verify gave undefined return value");
+    }
   }
-  if (!Buffer::HasInstance(args[0])) {
-    return VException("Argument 'sig' must be of type Buffer");
-  }
-  if (!Buffer::HasInstance(args[1])) {
-    return VException("Argument 'pubkey' must be of type Buffer");
-  }
-  if (!Buffer::HasInstance(args[2])) {
-    return VException("Argument 'hash' must be of type Buffer");
-  }
+};
 
-  v8::Handle<v8::Object> sig_buf = args[0]->ToObject();
-  v8::Handle<v8::Object> pub_buf = args[1]->ToObject();
-  v8::Handle<v8::Object> hash_buf = args[2]->ToObject();
+Persistent<FunctionTemplate> BitcoinKey::s_ct;
 
-  const unsigned char *sig_data = (unsigned char *) Buffer::Data(sig_buf);
-  const unsigned char *pub_data = (unsigned char *) Buffer::Data(pub_buf);
-  const unsigned char *hash_data = (unsigned char *) Buffer::Data(hash_buf);
-
-  unsigned int sig_len = Buffer::Length(sig_buf);
-  unsigned int pub_len = Buffer::Length(pub_buf);
-  unsigned int hash_len = Buffer::Length(hash_buf);
-
-  if (hash_len != 32) {
-    return VException("Argument 'hash' must be Buffer of length 32 bytes");
-  }
-
-  // Allocate pkey
-  pkey = EC_KEY_new_by_curve_name(NID_secp256k1);
-  if (pkey == NULL) {
-    return VException("Error from EC_KEY_new_by_curve_name");
-  }
-
-  // Load public key
-  if (!o2i_ECPublicKey(&pkey, &pub_data, pub_len)) {
-    return VException("Error from o2i_ECPublicKey(&pkey, pub_data, pub_len)");
-  }
-
-  // Verify signature
-  int result = ECDSA_verify(0, hash_data, hash_len, sig_data, sig_len, pkey);
-
-  // Free key
-  EC_KEY_free(pkey);
-
-  if (result == -1) {
-    return VException("Error during ECDSA_verify");
-  } else if (result == 0) {
-    // Signature invalid
-    return scope.Close(v8::Boolean::New(false));
-  } else if (result == 1) {
-    // Signature valid
-    return scope.Close(v8::Boolean::New(true));
-  } else {
-    return VException("ECDSA_verify gave undefined return value");
-  }
-}
 
 static Handle<Value>
 pubkey_to_address256 (const Arguments& args)
@@ -420,9 +754,8 @@ extern "C" void
 init (Handle<Object> target)
 {
   HandleScope scope;
-  target->Set(String::New("new_keypair"), FunctionTemplate::New(new_keypair)->GetFunction());
+  BitcoinKey::Init(target);
   target->Set(String::New("pubkey_to_address256"), FunctionTemplate::New(pubkey_to_address256)->GetFunction());
-  target->Set(String::New("verify_sig"), FunctionTemplate::New(verify_sig)->GetFunction());
   target->Set(String::New("base58_encode"), FunctionTemplate::New(base58_encode)->GetFunction());
   target->Set(String::New("base58_decode"), FunctionTemplate::New(base58_decode)->GetFunction());
   target->Set(String::New("sha256_midstate"), FunctionTemplate::New(sha256_midstate)->GetFunction());
